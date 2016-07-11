b7321baaef2e206559b74a662afa4b4c6732fc98
Matt Brennan <mattyb1000@gmail.com>
2016-07-11 17:46:52 +0100

    get repo from package.json instead

--- bin.js
+++ bin.js
@@ -3,27 +3,29 @@
 
 const infer = require('./');
 const spawn = require('@quarterto/spawn');
+const path = require('path');
+
+const packagePath = path.resolve('package.json');
+const {repository} = require(packagePath);
 
 if(process.env.npm_lifecycle_event && process.env.npm_lifecycle_event !== 'heroku-postbuild') {
-	console.log('⤼ Not a Heroku automatic deploy, skipping version inference');
+	console.log('⤼ not a Heroku automatic deploy, skipping version inference');
 	process.exit(0);
-} else if(!process.env.RECEIVE_DATA) {
-	console.log('¯\\_(ツ)_/¯ this is a Heroku automatic deploy so I was really hoping for some RECEIVE_DATA, but it\'s not there. Here\'s your env:');
+} else if(!repository) {
+	console.log('⊶ expected a repository field in your package.json');
+	process.exit(1);
+} else if(typeof repository !== 'string' || !repository.url) {
+	console.log('⊶ invalid repository entry in package.json, no url');
+	process.exit(1);
+} else if(!repository.type === 'git') {
+	console.log('⊶ non-git repositories not supported');
+	process.exit(1);
+} else if(!process.env.SOURCE_VERSION) {
+	console.log('⁈ supposedly this is a Heroku automatic deploy but there\'s no SOURCE_VERSION. Here\'s your env:');
 	console.log(process.env);
 	process.exit(1);
 } else {
-	const parseGithubUrl = url => {
-		const m = url.match(/github\.com\/([^\/]+)\/([^\/]+)(?:$|\/)/);
-		return m && {
-			user: m[1],
-			repo: m[2],
-		};
-	};
-
-	const herokuReceiveData = JSON.parse(process.env.RECEIVE_DATA);
-	const gh = parseGithubUrl(herokuReceiveData.deploy_source);
-	const cloneUrl = `https://github.com/${gh.user}/${gh.repo}.git`;
-	const version = infer(cloneUrl, process.env.SOURCE_VERSION, gh.repo);
+	const version = infer(typeof repository === 'string' ? repository : repository.url , process.env.SOURCE_VERSION, gh.repo);
 
 	console.log(`inferred version ${version}`);
 	spawn('npm', ['version', '--git-tag-version=false', version]);
